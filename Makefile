fe94c94db4229d0f3ca94eec5e8e6a30ea7a9968
Sebastian <sebablasko@gmail.com>
2015-03-03 14:46:08 -0300

    agrego archivos

---
+++ Makefile
@@ -0,0 +1,23 @@
+all: server client
+
+all_profiling: serverProfiling client
+
+server: server.o ../ssocket/ssocket.o
+	gcc server.o ../ssocket/ssocket.o -o server -lpthread
+
+serverProfiling: server.o ../ssocket/ssocket.o
+	gcc -fno-omit-frame-pointer -ggdb server.o ../ssocket/ssocket.o -o server -lpthread
+
+rm_server:
+	rm server server.o
+
+client: client.o ../ssocket/ssocket.o
+	gcc client.o ../ssocket/ssocket.o -o client -lpthread
+
+rm_client:
+	rm client client.o
+
+rm_ssocket:
+	rm ../ssocket/ssocket.o
+
+clean: rm_client rm_server rm_ssocket
---
+++ run.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+salida=threads_sockets_relation.csv
+MAX_PACKS=10000
+repetitions=4
+
+echo "Compilando..."
+make all
+echo "Done"
+
+echo "Ejecutando Prueba..."
+
+#Definir aquí el límite de sockets
+for num_sockets in {1..3}
+do
+	linea=$num_sockets";"
+
+	#Definir aqui el límite de Threads
+	for ((num_threads=1 ; $num_threads<=5 ; num_threads++))
+	{
+		echo "Evaluando "$num_threads" Threads y con "$num_sockets" Sockets"
+		for ((i=1 ; $i<=$repetitions ; i++))
+		{
+			if(($num_threads < $num_sockets))
+			then
+				echo "No se puede Evaluar"
+				linea=$linea"0, "
+			else
+				echo "Repeticion "$i
+				./server $MAX_PACKS $num_threads $num_sockets > aux &
+				pid=$!
+				sleep 1
+
+				./client $(($MAX_PACKS*10)) $num_sockets 127.0.0.1 > /dev/null &
+				./client $(($MAX_PACKS*10)) $num_sockets 127.0.0.1 > /dev/null &
+				./client $(($MAX_PACKS*10)) $num_sockets 127.0.0.1 > /dev/null &
+				./client $(($MAX_PACKS*10)) $num_sockets 127.0.0.1 > /dev/null &
+				wait $pid
+				linea="$linea$(cat aux)"
+				rm aux
+			fi
+		}
+		linea="$linea;"
+		echo ""
+	}
+	echo "$linea" >> $salida
+done
+make clean
+echo "Done"
+
+python postProcessing.py $salida
